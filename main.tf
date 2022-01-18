871be0d7fa37729a6cb35affccdf7c47c00105e4
tuborob <tuborob@MNL-B81Q8Y2.localdomain>
2022-01-18 16:26:54 +0800

    Deploy a single web server

--- main.tf
+++ main.tf
@@ -4,9 +4,25 @@ provider "aws" {
 resource "aws_instance" "simple" {
   ami		= "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-simple"
-    
+  }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
